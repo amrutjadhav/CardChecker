339a7cdf1d0a54cb294c05058995021ab5d89a83
Amrut Jadhav <amrutjadhav2294@gmail.com>
2018-09-18 20:42:19 +0900

    :bug:

--- app/message_template.js
+++ app/message_template.js
@@ -3,7 +3,7 @@ const template = {
     card: {
       titleWordCount: 'Please describe the ticket title in more descriptive way',
       titleTitleize: 'Title of ticket is not titliezed. Please make title titliezed',
-      descriptionAvailabilty: 'Ticket don\'t have description. Add some.'
+      descriptionAvailabilty: 'Ticket don\'t have description. Add some.',
       dueDate: 'Ticket don\'t have a due date. Take responsibility and ownership of your tasks.',
       labels: 'Ticket should have at least 2 labels. One for priority and other for classification.',
       members: 'Please assign the ticket to someone.'
