ab7dd3a25f0a755440bf45ce94745b5722480afa
Amrut Jadhav <amrutjadhav2294@gmail.com>
2018-10-15 15:42:19 +0900

    :bug:

--- app/message_template.js
+++ app/message_template.js
@@ -10,7 +10,7 @@ const template = {
       inProgressListMembersRequired: 'Card should be assigned to someone if it card is pushed to *In Progress*',
       listOfNewCard: 'Card should be created only in \'Task\' list.',
       checkListItemStateCompletion: (incompleteCount) => {
-        'When the card is moved to *In Review* list, all the checklist items in card should be get completed. There are ' + incompleteCount + 'items, which are not completed yet.'
+        return 'When the card is moved to *In Review* list, all the checklist items in card should be get completed. There are ' + incompleteCount + ' items, which are not completed yet.'
       },
       checkPullRequestAttachment: 'When card moves to *In Review* list, it should have pull request attached to it.'
     }
