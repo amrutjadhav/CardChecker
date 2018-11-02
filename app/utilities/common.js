e7e5867a9d04cd212a245f859a6012ec864e0794
Amrut Jadhav <amrutjadhav2294@gmail.com>
2018-11-02 15:42:15 +0900

    :sparkles: User can create own config file and it will override the default file.

--- app/utilities/common.js
+++ app/utilities/common.js
@@ -1,7 +1,14 @@
+const fs = require('fs')
 const pipelineConfig = require('../../default_pipeline_config.json')
 
 const commonUtilities = {
   getScopeConfig: (boardId) => {
+    // check if user defined a custom pipeline config file. If yes, merge with default pipeline config file.
+    if(fs.existsSync('../../pipeline_config.json')) {
+      let userDefinedConfig = require('../../pipeline_config.json')
+      pipelineConfig = Object.assign(pipelineConfig, userDefinedConfig)
+    }
+
     if(pipelineConfig.boards[boardId]) {
       return pipelineConfig.boards[boardId]
     } else {
