c3fa3458fddcaa285447a7cb1a497a11fdade0a1
Amrut Jadhav <amrutjadhav2294@gmail.com>
2018-11-02 15:49:17 +0900

    :bug: Avoid constant manipulation.

--- app/utilities/common.js
+++ app/utilities/common.js
@@ -3,16 +3,18 @@ const pipelineConfig = require('../../default_pipeline_config.json')
 
 const commonUtilities = {
   getScopeConfig: (boardId) => {
+    // Avoid manipulation of const.
+    let config = pipelineConfig
     // check if user defined a custom pipeline config file. If yes, merge with default pipeline config file.
     if(fs.existsSync('../../pipeline_config.json')) {
-      let userDefinedConfig = require('../../pipeline_config.json')
-      pipelineConfig = Object.assign(pipelineConfig, userDefinedConfig)
+      let userConfig = require('../../pipeline_config.json')
+      config = Object.assign(config, userConfig)
     }
 
-    if(pipelineConfig.boards[boardId]) {
-      return pipelineConfig.boards[boardId]
+    if(config.boards[boardId]) {
+      return config.boards[boardId]
     } else {
-      return pipelineConfig.company
+      return config.company
     }
   }
 }
