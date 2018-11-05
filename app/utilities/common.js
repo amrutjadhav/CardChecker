676c30e9fb219be76e699cb0661dd51d3c60988a
Amrut Jadhav <amrutjadhav2294@gmail.com>
2018-11-05 12:11:19 +0900

    merge configuration from yml file.

--- app/utilities/common.js
+++ app/utilities/common.js
@@ -1,24 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 const yaml = require('js-yaml')
-const pipelineConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../../default_pipeline_config.yml'), 'utf8'));
+const defaultPipelineConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../../default_pipeline_config.yml'), 'utf8'));
+const customPipelineConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../../pipeline_config.yml'), 'utf8'));
+const pipelineConfig = Object.assign(defaultPipelineConfig, customPipelineConfig)
 
 const commonUtilities = {
   getScopeConfig: (boardId) => {
-    // Avoid manipulation of const.
-    let config = pipelineConfig
-    // check if user defined a custom pipeline config file. If yes, merge with default pipeline config file.
-    if(fs.existsSync('../../pipeline_config.json')) {
-      let userConfig = require('../../pipeline_config.json')
-      config = Object.assign(config, userConfig)
-    }
-
-    if(config.boards && config.boards[boardId]) {
-      let boardConfig = config.boards[boardId]
-      // Merge company level default config with board config.
-      return Object.assign(config.company, boardConfig)
+    if(pipelineConfig.boards && pipelineConfig.boards[boardId]) {
+      let boardConfig = pipelineConfig.boards[boardId]
+      // Merge company level default config with board pipelineConfig.
+      return Object.assign(pipelineConfig.company, boardConfig)
     } else {
-      return config.company
+      return pipelineConfig.company
     }
   }
 }
