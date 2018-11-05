106848bd6521bcc54102f275df93bf5347f40333
Amrut Jadhav <amrutjadhav2294@gmail.com>
2018-11-06 00:13:05 +0900

    deepmerge config files.

--- app/utilities/common.js
+++ app/utilities/common.js
@@ -1,18 +1,24 @@
 const fs = require('fs')
 const path = require('path')
 const yaml = require('js-yaml')
-var pipelineConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../../default_pipeline_config.yml'), 'utf8'));
-if(fs.existsSync('../../pipeline_config.yml')) {
-  var customPipelineConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../../pipeline_config.yml'), 'utf8'));
-  pipelineConfig = Object.assign(pipelineConfig, customPipelineConfig)
+const deeptMerge = require('deepmerge')
+
+const overwriteMerge = (dest, target, options) => {
+  return target
 }
 
 const commonUtilities = {
   getScopeConfig: (boardId) => {
+    var pipelineConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../../default_pipeline_config.yml'), 'utf8'));
+    if(fs.existsSync(path.resolve(__dirname, '../../pipeline_config.yml'))) {
+      var customPipelineConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../../pipeline_config.yml'), 'utf8'));
+      pipelineConfig = deeptMerge(pipelineConfig, customPipelineConfig, { arrayMerge: overwriteMerge })
+    }
+
     if(pipelineConfig.boards && pipelineConfig.boards[boardId]) {
       let boardConfig = pipelineConfig.boards[boardId]
       // Merge company level default config with board pipelineConfig.
-      return Object.assign(pipelineConfig.company, boardConfig)
+      return deeptMerge(pipelineConfig.company, boardConfig, { arrayMerge: overwriteMerge })
     } else {
       return pipelineConfig.company
     }
